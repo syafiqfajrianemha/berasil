87a584dbae53c6b32d4afb288df437b2ff42f4f4
andiaditya162 <dityapra1234@gmail.com>
2023-11-28 10:13:40 +0800

    add font and color

---
+++ app/src/main/java/com/berasil/ui/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.berasil.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.berasil.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
