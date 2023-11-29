6640bacdf7c8910dd75e54ac5cfba15869642f93
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-11-29 16:51:47 +0700

    slicing login page

---
+++ app/src/main/java/com/berasil/customview/EditTextEmail.kt
@@ -0,0 +1,47 @@
+package com.berasil.customview
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import androidx.appcompat.widget.AppCompatEditText
+
+class EditTextEmail : AppCompatEditText {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (!Patterns.EMAIL_ADDRESS.matcher(text.toString()).matches()) {
+                    setError("Format email harus valid.", null)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
+    }
+}
--- app/src/main/java/com/berasil/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/berasil/ui/welcome/WelcomeActivity.kt
@@ -1,8 +1,10 @@
 package com.berasil.ui.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.berasil.databinding.ActivityWelcomeBinding
+import com.berasil.ui.login.LoginActivity
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -12,5 +14,10 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+            val toLoginPage = Intent(this, LoginActivity::class.java)
+            startActivity(toLoginPage)
+        }
     }
 }
