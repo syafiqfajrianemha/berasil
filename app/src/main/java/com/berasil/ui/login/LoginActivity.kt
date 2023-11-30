0bee4de3e2496a4e9648bcd8a0e96f731923d82d
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-11-30 12:13:54 +0700

    add validation button disabled

--- app/src/main/java/com/berasil/ui/login/LoginActivity.kt
+++ app/src/main/java/com/berasil/ui/login/LoginActivity.kt
@@ -2,6 +2,9 @@ package com.berasil.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import com.berasil.databinding.ActivityLoginBinding
 import com.berasil.ui.register.RegisterActivity
@@ -15,14 +18,32 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvDontHaveAccount.setOnClickListener {
-            val toRegisterPage = Intent(this, RegisterActivity::class.java)
-            startActivity(toRegisterPage)
-        }
+        binding.edLoginEmail.addTextChangedListener(loginTextWatcher)
+        binding.edLoginPassword.addTextChangedListener(loginTextWatcher)
 
         binding.buttonRegister.setOnClickListener {
             val toRegisterPage = Intent(this, RegisterActivity::class.java)
             startActivity(toRegisterPage)
+            finish()
+        }
+    }
+
+    private val loginTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            // Do nothing.
+        }
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            val edLoginEmail = binding.edLoginEmail.text.toString().trim()
+            val edLoginPassword = binding.edLoginPassword.text.toString().trim()
+
+            binding.loginButton.isEnabled =
+                Patterns.EMAIL_ADDRESS.matcher(edLoginEmail)
+                    .matches() && edLoginPassword.length >= 8
+        }
+
+        override fun afterTextChanged(s: Editable) {
+            // Do nothing.
         }
     }
 }
--- app/src/main/java/com/berasil/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/berasil/ui/register/RegisterActivity.kt
@@ -2,6 +2,9 @@ package com.berasil.ui.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import com.berasil.databinding.ActivityRegisterBinding
 import com.berasil.ui.login.LoginActivity
@@ -15,14 +18,34 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvHaveAccount.setOnClickListener {
-            val toLoginPage = Intent(this, LoginActivity::class.java)
-            startActivity(toLoginPage)
-        }
+        binding.edRegisterUsername.addTextChangedListener(registerTextWatcher)
+        binding.edRegisterEmail.addTextChangedListener(registerTextWatcher)
+        binding.edRegisterPassword.addTextChangedListener(registerTextWatcher)
 
         binding.buttonLogin.setOnClickListener {
             val toLoginPage = Intent(this, LoginActivity::class.java)
             startActivity(toLoginPage)
+            finish()
+        }
+    }
+
+    private val registerTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            // Do nothing.
+        }
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            val edRegisterUsername = binding.edRegisterUsername.text.toString().trim()
+            val edRegisterEmail = binding.edRegisterEmail.text.toString().trim()
+            val edRegisterPassword = binding.edRegisterPassword.text.toString().trim()
+
+            binding.registerButton.isEnabled =
+                edRegisterUsername.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(edRegisterEmail)
+                    .matches() && edRegisterPassword.length >= 8
+        }
+
+        override fun afterTextChanged(s: Editable) {
+            // Do nothing.
         }
     }
 }
