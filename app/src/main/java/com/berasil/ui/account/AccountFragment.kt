4605c34a3e56fa305e10e9f3221fc17df5b4e0db
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-12-02 10:42:10 +0700

    layouting account page

--- app/src/main/java/com/berasil/ui/account/AccountFragment.kt
+++ app/src/main/java/com/berasil/ui/account/AccountFragment.kt
@@ -1,11 +1,13 @@
 package com.berasil.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.berasil.databinding.FragmentAccountBinding
+import com.berasil.ui.welcome.WelcomeActivity
 
 class AccountFragment : Fragment() {
 
@@ -19,6 +21,15 @@ class AccountFragment : Fragment() {
     ): View {
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnToWelcomePage.setOnClickListener {
+            val toWelcomePage = Intent(activity, WelcomeActivity::class.java)
+            startActivity(toWelcomePage)
+        }
     }
 
     override fun onDestroyView() {
